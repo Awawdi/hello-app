497d6fb1fbd2a8a1fecbe05cb01a416ed52325cc
Orsan Awawdi <orsan.awawdi@gmail.com>
2024-07-19 15:54:10 +0300

    commit logs change

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
-import os
-
 from flask import Flask
-import logging
 
 app = Flask(__name__)
-log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs')
-os.makedirs(log_dir, exist_ok=True)
-logging.basicConfig(filename=os.path.join(log_dir, 'hello_world.log'), level=logging.INFO, format='%(asctime)s - %(message)s')
 
-
-@app.route("/")
+@app.route('/')
 def hello():
-    message = "Hello, Kubernetes!"
-    logging.info(message)
-    return message
+    return "Hello, World!"
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5050)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
