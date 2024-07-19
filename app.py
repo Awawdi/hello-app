1cad43acb5be6d8b57bb68a5da8f9920bc420100
Orsan Awawdi <orsan.awawdi@gmail.com>
2024-07-19 16:00:45 +0300

    added

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import logging
+import os
+
 from flask import Flask
 
 app = Flask(__name__)
 
+log_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'logs')
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=os.path.join(log_dir, 'hello_world.log'), level=logging.INFO,
+                    format='%(asctime)s - %(message)s')
+
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    message = "Hello, Kubernetes!"
+    logging.info(message)
+    return message
 
 
 if __name__ == '__main__':
